f3ac5f60fc3562d47e5133c7c5c6fe342b8bff0e
PhilipAtEmcogma <emcogma@gmail.com>
2024-04-02 01:54:14 +1300

    error handling excerise

--- src/m7_async.rs
+++ src/m7_async.rs
@@ -1,14 +1,24 @@
+use std::io::{Error, ErrorKind};
+
 
 //return either a json formatted structure, or an error message from reqwest library
-async fn my_async_call(url: &str) -> Result<serde_json::Value, reqwest::Error>{
-    
-    //await for waiting the async tasks to finish
-    let response: serde_json::Value = reqwest::get(url)
-    .await?
-    .json::<serde_json::Value>()
-    .await?;
+async fn my_async_call(url: &str) -> Result<serde_json::Value,  Error>{
 
-    Ok(response)
+   let response: reqwest::Response = reqwest::get(url)
+   .await
+   .map_err(|_| Error::new(ErrorKind::Other, "could not retrieve response"))?; //map one type of error to another using | | function
+
+   let json_response = response.json::<serde_json::Value>()
+                                    .await
+                                    .map_err(|_| Error::new(ErrorKind::Other, "could not decode to JSON"))?;
+    // solution for when using reqwest::Error library
+    // //await for waiting the async tasks to finish
+    // let response: serde_json::Value = reqwest::get(url)
+    // .await?
+    // .json::<serde_json::Value>()
+    // .await?;
+
+    Ok(json_response)
 }
 
 #[cfg(test)]
