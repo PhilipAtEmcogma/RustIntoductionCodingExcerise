76b3c9abaa9cbb9fde747926b99c536ceefe49a0
PhilipAtEmcogma <emcogma@gmail.com>
2024-04-06 17:15:58 +1300

    initial web server created

--- web_template/src/main.rs
+++ web_template/src/main.rs
@@ -91,6 +91,52 @@ impl Database{
 
 }
 
-fn main() {
-    println!("Hello, world!");
+//struct for share state. such that data can be shared, so multiple user can read it at the same time
+// reminder only one user can open a file at a time, thus in cases where multiple end user wants to read the open the same file
+// need to have the data shared
+struct AppState{
+    db: Mutex<Database>
+}
+
+//async function.  task: web::Json<Task> use to passing json data around
+async fn create_task(app_state: web::Data<AppState>, task: web::Json<Task>) -> impl Responder{
+    let mut db = app_state.db.lock().unwrap();
+    db.insert(task.into_inner());
+    let _ = db.save_to_file();
+    HttpResponse::Ok().finish() //reuturn ok as result, if file is save and closed successfully 
+}
+
+//writing a web server
+#[actix_web::main]
+async fn main() -> std::io::Result<()>{
+    let db = match Database::load_from_file() {
+        //an example of using match to either create a new database or return database
+        Ok(db) => db, //if database exist, return the database
+        Err(_) => Database::new() //if database didn't exist, instead of returning an error message, create it 
+    };
+
+    let data: web::Data<AppState> = web::Data::new(AppState {
+        db: Mutex::new(db)
+    });
+
+    // create http server
+    HttpServer::new(move || {
+        App::new()
+        .wrap(
+            Cors::permissive()
+            .allowed_origin_fn(|origin, _req_head|{
+                origin.as_bytes().starts_with(b"http://localhost") || origin == "null"
+            })
+            .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
+            .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
+            .allowed_header(header::CONTENT_TYPE)
+            .supports_credentials()
+            .max_age(3600)
+        )
+        .app_data(data.clone())
+        .route("/task", web::post().to(create_task))
+    })
+    .bind("127.0.0.1:8080")?
+    run()
+    .await
 }
